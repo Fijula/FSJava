fb237e66d272d3171a948ac9b32c593ad041fda3
fijula <fijulaazhikoden123@gmail.com>
2024-03-22 09:55:49 +0530

    ADD : First commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,10 @@
+package org.example;
+//First Program to print name
+public class App
+{
+    public static void main( String[] args )
+    {
+        String name = "Fijula";
+        System.out.println( "My name is "+name );
+    }
+}
