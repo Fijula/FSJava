a72623155776b2554ba0ea7a7706ff766c5cab7d
fijula <fijulaazhikoden123@gmail.com>
2024-03-31 13:28:45 +0530

    ADD : File

--- src/main/java/JavaPractises.java
+++ src/main/java/JavaPractises.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class JavaPractises {
+public class JavaPractises {
+
+    public static void main(String[] args) {
+        JavaPractises jd = new JavaPractises();
+        System.out.println(jd.addNum(1,2));
+        System.out.println(jd.subNum(5,2));
+        System.out.println(jd.divideNum(5,2));
+        System.out.println(jd.multNum(1,2));
+
+
+    }
+    public int addNum(int a,int b)
+    {
+        return a+b;
+    }
+    public int subNum(int a,int b)
+    {
+        return a-b;
+    }
+    public float divideNum(int a,int b)
+    {
+        return ((float)(a /b));
+    }
+    public int multNum(int a,int b)
+    {
+        return a*b;
+    }
 }
+
+
+
